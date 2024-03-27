c042916576e45f52ec6f160b9c7bfc947d29f90b
SPAUPA <40449398+sinibida@users.noreply.github.com>
2024-03-27 03:21:46 +0000

    follow04

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
-import './App.css';
+import Content from './Content';
+import Footer from './Footer';
+import Header from './Header';
 
 function App() {
   return (
-    <div>
-      Hello World
+    <div className='app'>
+      <Header/>
+      <Content/>
+      <Footer/>
     </div>
   );
 }
