aa9499aa78537ef2698bfb97f3e0e463e2c699f9
SPAUPA <40449398+sinibida@users.noreply.github.com>
2024-03-27 03:32:41 +0000

    Ch5 Click Events

--- src/Content.jsx
+++ src/Content.jsx
@@ -1,9 +1,32 @@
 import React from 'react'
 
 function Content() {
+  const handleNameChange = () => {
+    const names = ['SPAUPA', 'Juna', 'Yahiamice'];
+    const num = Math.floor(Math.random() * 3);
+    return names[num];
+  }
+
+  const handleClick = () => {
+    console.log("Clicked it!");
+  }
+
+  const handleClick2 = (name) => {
+    console.log(`${name} clicked it!`);
+  }
+
+  const handleClick3 = (e) => {
+    console.log(`${e.target.innerText} clicked it!`);
+  }
+
   return (
     <div className='content'>
-        Content
+        <p>
+          Hello {handleNameChange()}
+        </p>
+        <button onClick={handleClick}>Click 1</button>
+        <button onClick={() => handleClick2("SPAUPA")}>Click 2</button>
+        <button onClick={(e) => handleClick3(e)}>ClickytyClacky</button>
     </div>
   )
 }
