b77b091365f9b919cb700a493c34c26d1fb3d317
SPAUPA <40449398+sinibida@users.noreply.github.com>
2024-03-27 03:45:45 +0000

    Ch6 ClickityClacky

--- README.md
+++ README.md
@@ -4,3 +4,24 @@ element의 click event를 다룸
 
 `onClick={...}`
 !!: `e.target.innerText`
+
+# Chapter 6 useState
+
+```jsx
+const handleClick = () => {
+  setCount(count + 1); // <== Not added immediately
+  setCount(count + 1); // <== Not added immediately
+
+  console.log(count); 
+  // ex: if count was = 0 before `setCount(...)`
+  // console will print `0` instead of `1`
+
+  // multiple setCount 증감 => 한 번만 적용됨!
+}
+```
+
+# !!! Chapter 7 Lists & Keys
+
+Grocery List의 각 목록을 구현하며 List를 React에서 사용하는 법을 배운다.
+
+
--- src/Content.jsx
+++ src/Content.jsx
@@ -1,14 +1,25 @@
 import React from 'react'
+import { useState } from 'react';
 
 function Content() {
+  const [name, setName] = useState("SPAUPA");
+  const [count, setCount] = useState(0);
+
   const handleNameChange = () => {
     const names = ['SPAUPA', 'Juna', 'Yahiamice'];
     const num = Math.floor(Math.random() * 3);
-    return names[num];
+    setName(names[num]);
   }
 
   const handleClick = () => {
-    console.log("Clicked it!");
+    setCount(count + 1); // <== Not added immediately
+    setCount(count + 1); // <== Not added immediately
+
+    console.log(count); 
+    // ex: if count was = 0 before `setCount(...)`
+    // console will print `0` instead of `1`
+
+    // multiple setCount 증감 => 한 번만 적용됨!
   }
 
   const handleClick2 = (name) => {
@@ -21,10 +32,10 @@ function Content() {
 
   return (
     <div className='content'>
-        <p>
-          Hello {handleNameChange()}
+        <p onDoubleClick={handleClick}>
+          Hello {name} / Count = {count}
         </p>
-        <button onClick={handleClick}>Click 1</button>
+        <button onClick={handleNameChange}>Click 1</button>
         <button onClick={() => handleClick2("SPAUPA")}>Click 2</button>
         <button onClick={(e) => handleClick3(e)}>ClickytyClacky</button>
     </div>
